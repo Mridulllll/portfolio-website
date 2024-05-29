72d3f7000d232b4d7ac9cf6d397cd65744da41f4
Mridul Neupane <ballapple39@gmail.com>
2024-05-29 13:42:59 +0545

    Home page

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //Sections active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //Remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //Hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
